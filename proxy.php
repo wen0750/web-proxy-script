b92cafa63559484a47e95ef3c8df8fddaac12b2b
wen0750 <wenjia2630213722@gmail.com>
2025-05-05 21:56:35 +0800

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -3,6 +3,7 @@
 // Private web proxy script by Heiswayi Nrird (https://heiswayi.nrird.com)
 // Released under MIT license
 // Free Software should work like this: whatever you take for free, you must give back for free.
+
 if (!function_exists("gzcompress")) die ("This proxy requires PHP's zlib extension. Please install/enable it on your server and try again.");
 if (!function_exists("curl_init")) die ("This proxy requires PHP's cURL extension. Please install/enable it on your server and try again.");
 
